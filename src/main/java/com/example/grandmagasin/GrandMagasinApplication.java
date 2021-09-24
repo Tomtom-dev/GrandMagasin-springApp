75e8cc4bfb36680395c383a6b97dd5f2e1335f71
thomas Heddadji <thomasheddadji0@gmail.com>
2021-09-24 09:43:52 +0200

    create the folder

---
+++ src/main/java/com/example/grandmagasin/GrandMagasinApplication.java
@@ -0,0 +1,13 @@
+package com.example.grandmagasin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandMagasinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrandMagasinApplication.class, args);
+    }
+
+}
