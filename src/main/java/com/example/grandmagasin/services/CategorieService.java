d3e8a91a6f8509fcce2476bb958b03b21bcfcad7
thomas Heddadji <thomasheddadji0@gmail.com>
2021-09-24 14:35:56 +0200

    add categorieService and impl

--- src/main/java/com/example/grandmagasin/services/CategorieService.java
+++ src/main/java/com/example/grandmagasin/services/CategorieService.java
@@ -6,11 +6,11 @@ import com.example.grandmagasin.models.Produit;
 import java.util.List;
 import java.util.Optional;
 
-public interface CategoryRepository {
+public interface CategorieService {
 
     public List<Categorie> TrouverTousClients();
     public Optional<Categorie> findById(Long aLong);
 
     public void deleteById(long aLong);
-    public Produit save (Produit entity);
+    public Categorie save (Categorie entity);
 }
